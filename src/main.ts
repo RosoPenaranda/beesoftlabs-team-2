bb6e08c8e1ab8280e491c5bac8394d876049f03b
DiegoMorales <diego.mm09@hotmail.com>
2022-11-10 15:18:47 -0500

    Delete a comment at app.module

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule /* { logger: false } */);
+  const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('/Main');
   const appConfigService: AppConfigService = app.get(AppConfigService);
